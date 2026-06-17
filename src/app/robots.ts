a18cd18a1e6ca27f3880ccc375ed84d0d427a332
sbornovmax <sbornov97@gmail.com>
2026-06-17 12:04:04 +0400

    chore: закрыть страницу входа в админку от индексации

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      { userAgent: '*', allow: '/', disallow: ['/admin/', '/account/', '/checkout/'] },
+      { userAgent: '*', allow: '/', disallow: ['/admin/', '/admin-login/', '/account/', '/checkout/'] },
       { userAgent: 'Yandex', allow: '/' },
     ],
     sitemap: 'https://iceline-pro.vercel.app/sitemap.xml',
